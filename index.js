33d2ba960714a856e8abf3041aab100ac8fc7214
Ahmad Muzaki Salman Alfaris <ahmadmuzaki.s.a@gmail.com>
2023-01-20 09:18:30 +0700

    change desc

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require('express')()
 
-app.get('/',(req, res)=>{
-    res.send("Welcome to the abyss")
+app.get('/', (req, res) => {
+    res.send("Welcome to the abyss this my land")
 })
 
-app.listen(8000, ()=>{
+app.listen(8000, () => {
     console.log('Serber berjalan pada port 8000')
 })
